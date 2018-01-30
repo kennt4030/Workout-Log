75b672af9a3ceae022921fd26cc98d1d2c8093a1
Kenny <kenny47201@gmail.com>
2018-01-30 10:27:48 -0500

    checking

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,20 +1,33 @@
 //Here we’re importing the express module with the first variable. Then, we are calling the express function in the app variable. This function puts a new Express application inside the app variable. This is creating an instance of Express that we can use to call the various Express functions that will help us build our server.//
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db');
+var User = sequelize.import('./models./user');
 
-var User = sequelize.import(__dirname + '\\models\\user');
 //Create table
 User.sync(); // sync( {force: true}), to drop then create each time the app starts!
 
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
+// creating a user
 app.use('/api/user', require('./routes/user'));
+// logging in a user
+app.use('/api/login', require('./routes/session'));
+// localhost:3000/api/login/
+app.use('/app/definition', require('./routes/definition'));
+
+app.use('/api/log', require('./routes/log'));
+
 app.use('/api/test', function(req, res){
-	res.send("Hello World");
+	res.send("<h1>Hello World</h1>");
 });
 
 app.listen(3000, function(){
 	console.log('App is listening on 3000.')
 });
+
+
+
--- workoutlogServer/routes/user.js
+++ workoutlogServer/routes/user.js
@@ -1,11 +1,8 @@
-import { Session } from 'inspector';
-
 var router = require('express').Router();
 var sequelize = require('../db.js');
 var User = sequelize.import('../models/user');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
-
 
 router.post('/', function(req, res) {
 		var username = req.body.user.username;
@@ -15,15 +12,16 @@ router.post('/', function(req, res) {
 
 		User.create({
 			username: username,
-			passwordhash: bcrypt.hashSync(pass,10)
+			passwordhash: bcrypt.hashSync(pass, 10)
 		}).then(
 		//Sequelize is going to return the object it created from db.
 
 			function createSuccess(user){
-				var token = jwt.sign({id:user.id}, 'i_am_secret', {expiresIn: 60*60*24})
+				var token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
+
 				res.json({
 						user: user,
-						message: 'create',
+						message: 'created',
 						sessionToken: token
 				});
 			},
